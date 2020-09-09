74991a11563dea760ba526f33c7d011f37f26d80
guoxu <765452843@qq.com>
2020-09-09 20:20:21 +0800

    tmp

--- pdd03.cpp
+++ pdd03.cpp
@@ -1,36 +1,41 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-typedef pair<int, int> pii;
+#include <iostream>
+typedef pair<int, int> pa;
 
-int dp[202][5005];
-pii a[202];
+int dp[220][5050];
+pa a[202];
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
-    int nm = m;
-    for (int i = 1; i <= n; ++i) {
+    cin>>n>>m;
+    int res = 0;
+
+
+    int t = m;
+    for (int i = 1; i <= n; i++) {
         scanf("%d%d", &a[i].first, &a[i].second);
+        cin>>a[i].first>>a[i].second;
         if (a[i].first < 0) {
-            nm -= a[i].first;
+            t -= a[i].first;
         }
     }
-    sort(a + 1, a + 1 + n, greater<pii>());
+    sort(a + 1, a + 1 + n, greater<pa>());
     for (int i = 1; i <= n; ++i) {
         if (a[i].first > 0) {
-            for (int j = a[i].first; j <= nm; ++j) {
+            for (int j = a[i].first; j <= t; ++j) {
                 dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i].first] + a[i].second);
             }
         }
         else {
-            for (int j = 0; j <= nm + a[i].first; ++j) {
+            for (int j = 0; j <= t + a[i].first; j++) {
                 dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i].first] + a[i].second);
             }
         }
     }
-    int ans = 0;
     for (int i = 0; i <= m; ++i) {
-        ans = max(ans, dp[n][i]);
+        res = max(res, dp[n][i]);
     }
-    printf("%d\n", ans);
+//    printf("%d\n", res);
+    cout<<ans<<endl;
     return 0;
