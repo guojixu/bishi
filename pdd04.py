bd50f71ab6aec67f49d98b035a9e3affc86d9451
qjh <me@home.com>
2020-09-09 20:19:35 +0800

    4

--- pdd04.py
+++ pdd04.py
@@ -1,15 +1,35 @@
-n, m = [int(_) for _ in input().split()]
+def kmp(mom_string, son_string):
+    pos1 = []
+    test = ''
+    if type(mom_string) != type(test) or type(son_string) != type(test):
+        pos1.append(-1)
+        return pos1
+    if len(son_string) == 0:
+        pos1.append(0)
+        return pos1
+    if len(mom_string) == 0:
+        pos1.append(-1)
+        return pos1
+    next = [-1] * len(son_string)
+    if len(son_string) > 1:
+        next[1] = 0
+        i, j = 1, 0
+        while i < len(son_string) - 1:
+            if j == -1 or son_string[i] == son_string[j]:
+                i += 1
+                j += 1
+                next[i] = j
+            else:
+                j = next[j]
+    m = s = 0
+    while (s < len(son_string) and m < len(mom_string)):
+        if s == -1 or mom_string[m] == son_string[s]:
+            m += 1
+            s += 1
+        else:
+            s = next[s]
 
-nums = []
-nm = m
-for _ in range(n):
-    nums.append([int(_) for _ in input().split()])
-    if nums[0] < 0:
-        nm -= nums[0]
-
-nums.sort(reverse=True)
-dp = [[0] * (m+1) for _ in range(n+1)]
-for i in range(1, n + 1):
-    if nums[0] > 0:
-        for j in range(nums[i][0], nm + 1):
-            dp[i][j]=max(dp[i-1][j],dp[i-1][j-nums[i][0]])
+    if s == len(son_string):
+        pos1.append(m - s)
+    pos1.append(-1)
+    return pos1
